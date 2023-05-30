0643b47efd4819d2c0e04b1e9bfdd246f8f55395
Brian <oshinbolubrian@gmai;.com>
2023-05-30 11:11:37 +0100

    template done

---
+++ src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default navbar;
---
+++ src/pages/login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>login Page</div>
+  )
+}
+
+export default login
