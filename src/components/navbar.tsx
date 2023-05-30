c0e61f2d4648317463f0f3a6b3620cbbb36d548a
Brian <oshinbolubrian@gmai;.com>
2023-05-30 16:29:05 +0100

    firebase user login donw

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
-const navbar = () => {
+const Navbar = () => {
+  const [user] = useAuthState(auth);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
+
   return (
-    <div>
-      <Link to="/"> Home </Link>
-      <Link to="/login"> Login </Link>
+    <div className="navbar">
+      <div className="links">
+        <Link to="/"> Home </Link>
+        <Link to="/login"> Login </Link>
+      </div>
+
+      <div className="user">
+        {user && (
+          <>
+            <p>{user?.displayName}</p>
+            <img src={user?.photoURL || ""} alt="" width="50" height="50" />
+            <button onClick={signUserOut}>Log Out</button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
 
-export default navbar;
+export default Navbar;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import{ auth, provider} from "../config/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-const login = () => {
+
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const signinWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider)
+    console.log(result);
+    navigate("/");
+  }
+
+
   return (
     <div>
-      
-      <p>Sign in with Google to continue</p>
+      <p>Sign in with Google To Continue</p>
+      <button onClick={signinWithGoogle}>Sign in with Google</button>
     </div>
   );
 };
 
-export default login;
+export default Login;
